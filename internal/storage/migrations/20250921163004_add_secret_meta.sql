26d68efeff0d0427f68a202649b8ce347498b153
Morozov Denis <denmor86@gmail.com>
2025-09-21 21:37:21 +0300

    fix secret migration

--- internal/storage/migrations/20250921163004_add_secret_meta.sql
+++ internal/storage/migrations/20250921163004_add_secret_meta.sql
@@ -1,15 +1,12 @@
 -- +goose Up
 -- +goose StatementBegin
 ALTER TABLE secrets
-ADD COLUMN created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
 ADD COLUMN updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW();
 
 -- +goose StatementEnd
 
 -- +goose Down
 -- +goose StatementBegin
-ALTER TABLE secrets
-DROP COLUMN IF EXISTS created_at;
 ALTER TABLE secrets
 DROP COLUMN IF EXISTS updated_at;
 -- +goose StatementEnd
